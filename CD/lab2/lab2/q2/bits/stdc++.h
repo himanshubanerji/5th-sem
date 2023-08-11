cda7a844b6d16a1facfd255102fe13ee12073931
Himanshu Banerji <42094721+himanshubanerji@users.noreply.github.com>
2023-08-11 11:23:28 +0530

    Add files via upload

---
+++ CD/lab2/lab2/q2/bits/stdc++.h
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
---
+++ CD/lab2/lab2/q3/bits/stdcc+.h
@@ -0,0 +1,7 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
